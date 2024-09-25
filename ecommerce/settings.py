e4a720372aae98b1ffdf34cbfdea8194b6cdc961
Emmanuel <eroza9792@gmail.com>
2024-09-25 11:29:38 +0300

    settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -144,7 +144,7 @@ import django_heroku
 #STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
