6a91a4df5a25301414763afff66fa325c0fd3747
Emmanuel <eroza9792@gmail.com>
2024-09-26 23:04:29 +0300

    last

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("ecommerceapp.urls")),
     path('auth/', include("authcart.urls")),   
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
